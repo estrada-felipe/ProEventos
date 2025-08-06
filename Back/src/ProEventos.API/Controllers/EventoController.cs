e395108e34bba387254411ac4a088d088abe9070
FelipeEstrada <festrada@custec.com.br>
2025-08-06 17:48:20 -0300

    Entity Framework Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -13,54 +15,28 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-
-        public IEnumerable<Evento> _evento = new Evento[] {
-            new Evento() {
-            EventoId = 1,
-            Local = "Local do Evento",
-            DataEvento = DateTime.Now.AddDays(1),
-            Tema = "Tema do Evento",
-            QtdPessoas = 100,
-            Lote = "Lote 1",
-            ImagemUrl = "http://localhost:5000/imagens/evento1.jpg",
-            Telefone = "(11) 99999-9999",
-            Email = ""
-            },
-            new Evento(){
-                EventoId = 2,
-                Local = "Local do Evento",
-                DataEvento = DateTime.Now.AddDays(1),
-                Tema = "Tema do Evento",
-                QtdPessoas = 100,
-                Lote = "Lote 1",
-                ImagemUrl = "http://localhost:5000/imagens/evento1.jpg",
-                Telefone = "(11) 99999-9999",
-                Email = ""
-            }
-        };
-
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;    
+            return _context.Eventos;    
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public Evento Get(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);    
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
 
 
 
-
-
+ 
 
 
 
